b27f06c2a6bd96c4169e382ce0850fd48042cff0
andrea.palazzi <andrea.palazzi@nomitri.com>
2021-03-08 18:22:03 +0100

    Add scripts for fresh install bootstrapping

---
+++ bin/snap_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo snap install $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+install ffmpeg
+install gimp
+install vlc
+
