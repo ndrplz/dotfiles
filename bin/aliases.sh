ee28e16478308409e84f0a94b8698e9fb8920ac4
andrea.palazzi <andrea.palazzi@nomitri.com>
2021-03-11 20:00:10 +0100

    Add basic aliases

---
+++ bin/aliases.sh
@@ -0,0 +1,7 @@
+# Basics
+alias ll='ls -alF'
+alias la='ls -A'
+
+# Internet
+alias myip="curl https://ipinfo.io/json" # or /ip for plain-text ip
+alias speedcheck="curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -"
