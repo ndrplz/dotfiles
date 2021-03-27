39c157f90c1032d1938b8fa725757e25e4788841
andrea.palazzi <andrea.palazzi@nomitri.com>
2021-03-27 19:53:33 +0100

    Add lf

--- bin/functions.sh
+++ bin/functions.sh
@@ -1,3 +1,16 @@
 function serve {
 	python3 -m http.server $1	
 }
+
+# Use lf to switch directories and bind it to ctrl-o
+# https://www.youtube.com/watch?v=eLEo4OQ-cuQ
+lfcd () {
+    tmp="$(mktemp)"
+    lf -last-dir-path="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+    fi
+}
+bindkey -s '^o' 'lfcd\n'
